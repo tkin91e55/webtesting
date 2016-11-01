cd2576cac86c1f739ae24eb9eafc3f8e6251a11e
tsangkk <t.kingless@yahoo.com>
2016-11-01 11:33:14 +0800

    link_crawler() avoid repeated crawling

--- download.py
+++ download.py
@@ -34,6 +34,8 @@ def link_crawler(seed_url, link_regex):
     """Crawl from the given seed URL following links matched by link_regex
     """
     crawl_queue = [seed_url]
+    # keep track which URL's have seen before
+    seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
         html = download(url)
@@ -41,8 +43,10 @@ def link_crawler(seed_url, link_regex):
         for link in get_links(html):
             if re.match(link_regex, link):
                 link = urlparse.urljoin(seed_url, link)
-                crawl_queue.append(link)
-                print str(link)
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
+                    print str(link)
 
 
 def get_links(html):
