9a985fbd832dfa4ff1c6721c83b4df0c5fb20008
tsangkk <t.kingless@yahoo.com>
2016-11-01 10:10:02 +0800

    download add user agent header

--- download.py
+++ download.py
@@ -1,8 +1,10 @@
 import urllib2
 
 
-def download(url, num_retries=2):
+def download(url, user_agent='wswp', num_retries=2):
     print 'Downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
     try:
         html = urllib2.urlopen(url).read()
         print html
