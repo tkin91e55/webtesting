bd9e298dbd60b8290aef50f4594ae343c890a673
tsangkk <t.kingless@yahoo.com>
2016-11-01 12:22:43 +0800

    add proxy futuristic functions

--- download.py
+++ download.py
@@ -4,10 +4,17 @@ import urlparse
 from parseRobotTxt import rp, user_agent
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='wswp', proxy=None, num_retries=2):
     print 'Downloading:', url
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
+
+    opener = urllib2.build_opener()
+
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
+
     try:
         html = urllib2.urlopen(url).read()
         # print html
@@ -17,7 +24,7 @@ def download(url, user_agent='wswp', num_retries=2):
         if num_retries > 0:
             # retry 5XX HTTP error
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, num_retries - 1)
+                return download(url, user_agent, proxy, num_retries - 1)
     return html
 
 
