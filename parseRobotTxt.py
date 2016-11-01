22195cf7b41efafc8a346e13940f1a5295fd02de
tsangkk <t.kingless@yahoo.com>
2016-11-01 11:40:55 +0800

    add parseRobotTxt.py

---
+++ parseRobotTxt.py
@@ -0,0 +1,12 @@
+import robotparser
+
+
+rp = robotparser.RobotFileParser()
+rp.set_url('http://example.webscraping.com/robots.txt')
+rp.read()
+url = 'http://example.webscraping.com'
+user_agent = 'BadCrawler'
+print rp.can_fetch(user_agent, url)
+
+user_agent='GoodCrawler'
+print rp.can_fetch(user_agent, url)
